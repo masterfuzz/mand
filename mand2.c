9802e50887708fef47db8ae12e5759b3fa742ebd
Robert Martens <master.fuzz@gmail.com>
2012-11-24 16:57:34 -0500

    added command line options and fixed mkr script

--- mand2.c
+++ mand2.c
@@ -2,10 +2,12 @@
 #include <stdio.h>
 #include <math.h>
 #include <pthread.h>
+#include <unistd.h>
 //#include <png.h>
 
 #define MAX_ITER	500
 #define NTHREADS	8
+#define DELTA		0.0000001
 //#define PVALUE		2.5
 
 int escape(double re, double im);
@@ -16,8 +18,10 @@ u_int16_t* pixmap;
 double center_re = 0;
 double center_im = 0;
 double scale = 1;
-double pvalue = 3;
+double pvalue = 2;
+int efficient_power = 1;
 int hcells = 500;
+int max_iter = MAX_ITER;
 
 void *main_loop(void *arg)
 {
@@ -28,7 +32,7 @@ void *main_loop(void *arg)
 	for (i = *offset; i < hcells*2; i += NTHREADS) {
 		for (j = 0; j < hcells*2; j++) {
 			// the case for p=2 is way faster (optimized)
-			if (pvalue == 2) {
+			if (efficient_power) {
 				color = escape(
 					(i - hcells) / scale + center_re,
 					(j - hcells) / scale - center_im);
@@ -42,7 +46,7 @@ void *main_loop(void *arg)
 //			if (color == MAX_ITER)
 //				points++;
 
-			pixmap[j*(hcells*2) + i] = (MAX_ITER < 65536) ? color * 65535 / MAX_ITER : color % 65536;
+			pixmap[j*(hcells*2) + i] = (max_iter < 65536) ? color * 65535 / max_iter : color % 65536;
 
 //			total += color;
 
@@ -63,11 +67,47 @@ int main(int argc, char *argv[])
 	// -n		max iterations
 	// -c		cells
 	// -p		power
+	int c = 0;
+	char *outfile = "out";
+	while ((c = getopt(argc, argv, "x:y:z:n:c:p:f:")) != -1) {
+		double optarg_v = strtod(optarg, NULL);
+		switch (c) {
+		case 'x':
+			center_re = optarg_v;
+			break;
+		case 'y':
+			center_im = optarg_v;
+			break;
+		case 'z':
+			scale = optarg_v;
+			break;
+		case 'n':
+			max_iter = (int)optarg_v;
+			break;
+		case 'c':
+			hcells = (int)optarg_v;
+			break;
+		case 'p':
+			pvalue = optarg_v;
+			if (fabs(pvalue - 2.0) < DELTA) {
+				efficient_power = 1;
+			} else {
+				efficient_power = 0;
+			}
+			break;
+		case 'f':
+			outfile = optarg;
+			break;
+		default:
+			fprintf(stderr, "Invalid option '-%c'\n", optopt);
+			exit(1);
+		}
+	}
 
 
 	printf("Mandelbrot at (%f, %f)\nScale: %fx\nMax iterations: %d\nCells: %d^2\n",
 		center_re, center_im,
-		scale, MAX_ITER, hcells*2);
+		scale, max_iter, hcells*2);
 
 	// alocate pixel array
 	pixmap = malloc(sizeof(int) * 4 * hcells * hcells);
@@ -102,7 +142,7 @@ int main(int argc, char *argv[])
 
 	printf("Writing to file... ");
 	FILE *fp;
-	fp = fopen("out", "w");
+	fp = fopen(outfile, "w");
 
 	fwrite(pixmap, sizeof(u_int16_t), hcells*hcells*4, fp);		
 	fclose(fp);
@@ -125,7 +165,7 @@ int escape(double re0, double im0)
 	re = 0; im = 0;
 //	re =  .1; im = .7;
 
-	while (iter < MAX_ITER && re * re + im * im < 4) {
+	while (iter < max_iter && re * re + im * im < 4) {
 		// z = z^2 + c
 		tmp = re * re - im * im + re0;
 		im = 2 * re * im + im0;
@@ -145,7 +185,7 @@ int escape_c(double re0, double im0)
 	double re, im, tmp;
 	re = re0; im = im0;
 
-	while (iter < MAX_ITER && re * re + im * im < 4) {
+	while (iter < max_iter && re * re + im * im < 4) {
 		// z = z^2
 		tmp = re * re - im * im;
 		im = 2 * re * im;
@@ -167,7 +207,7 @@ int escape_p(double re0, double im0, double p)
 	double z_arg = 0;
 	re = 0; im = 0;
 
-	while (iter < MAX_ITER && re * re + im * im < 4) {
+	while (iter < max_iter && re * re + im * im < 4) {
 		// z = z^p + c
 		// z = r^p exp(I * arg * p) + c
 		z_mod = pow(re * re + im * im, p / 2);
