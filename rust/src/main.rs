f36ba22d07b5042d6963f22382c0b9c8c92b5186
Rob Martens <master.fuzz@gmail.com>
2018-06-14 20:47:02 -0400

    rust cargo project. working on image output

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,5 @@
+extern crate image;
+use image::{DynamicImage, ImageBuffer};
 
 const MAX_ITER: i32 = 500;
 const CENTER_R: f64 = 0.0;
@@ -8,23 +10,31 @@ const N_THREADS: u32 = 1;
 
 
 fn main() {
+  let mut img = ImageBuffer::new(CELLS*2, CELLS*2);
   println!("Hello! {}", escape(1.0, 1.0));
-  main_loop(0);
+  main_loop(0, &img);
   println!("Done!");
 }
 
-fn main_loop(offset: u32) {
+fn main_loop(offset: u32, img: *mut DynamicImage) {
   let mut i = offset;
 
   while i < CELLS*2 {
     for j in 0..(CELLS*2) {
-      escape(
+      let e = escape(
         (i as f64 - CELLS as f64) / SCALE + CENTER_R,
         (j as f64 - CELLS as f64) / SCALE - CENTER_I
       );
+      set_pixel(img, i, j, e);
     }
     i += N_THREADS;
   }
+}
+
+fn set_pixel(img: *mut DynamicImage, i: u32, j: u32, e: u32) {
+  img.put_pixel(i, j, image::Luma([
+    (e % 255) as u8
+  ]))
 }
 
 fn escape(re0: f64, im0: f64) -> i32 {
