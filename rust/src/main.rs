445a4fd59d613c45485a3e420d130dd511cecb4c
Rob Martens <master.fuzz@gmail.com>
2018-06-17 13:45:33 -0400

    using rayon library for concurrency

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,6 +1,8 @@
 extern crate image;
 extern crate num_complex;
+extern crate rayon;
 use num_complex::Complex;
+use rayon::prelude::*;
 
 const MAX_ITER: u32 = 2000;
 const CENTER_R: f64 = -0.748;
@@ -12,22 +14,28 @@ const L255: f64 = 367.88723542668566888;
 
 
 fn main() {
-  let mut img = image::GrayImage::new(CELLS*2, CELLS*2);
+  //let mut img = image::GrayImage::new(CELLS*2, CELLS*2);
   let cscale = SCALE * CELLS as f64;
 
   println!("Running...");
-  for (x, y, pixel) in img.enumerate_pixels_mut() {
+  //img.enumerate_pixels_mut()
+  //   .for_each(|(x, y, pixel): (u32, u32, &mut image::Luma<u8>)| {
+  let buf: Vec<_> = (0..(CELLS*CELLS*4)).into_par_iter()
+                      .map(|i| {
+    let x = i / (CELLS*2);
+    let y = i % (CELLS*2);
     let e = escape(Complex::new(
       (x as f64 - CELLS as f64) / cscale + CENTER_R,
       (y as f64 - CELLS as f64) / cscale - CENTER_I
     ));
 
     //*pixel = image::Luma([(e % 255) as u8]);
-    *pixel = image::Luma([f64::floor(L255 * f64::log2(1f64 + e as f64 / MAX_ITER as f64)) as u8]);
-  }
+    return f64::floor(L255 * f64::log2(1f64 + e as f64 / MAX_ITER as f64)) as u8;
+  }).collect();
 
   println!("Saving");
-  img.save("out.png").unwrap();
+  let img = image::GrayImage::from_raw(CELLS*2, CELLS*2, buf);
+  img.unwrap().save("out.png").unwrap();
 
   println!("Done");
 }
